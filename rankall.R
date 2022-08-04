97dead85c357c8231c65e918104e0d32ec1296b0
PollyHung <105984864+PollyHung@users.noreply.github.com>
2022-08-04 04:01:31 +0100

    Add files via upload

---
+++ rankall.R
@@ -0,0 +1,61 @@
+###rankall takes 2 arguments outcome name (outcome) and hospital rank (num). 
+###returns 2 colun data frame containing the hospital in each state that has 
+#the ranking specified in num. the function should return a value for every state 
+
+rankall <- function(outcome, num="best") {
+ 
+  ########set the working directory 
+  setwd("~/Desktop/Summer school/Data science [Specialization]/R programming /Week 4/rprog_data_ProgAssignment3-data")
+  library(magrittr)
+  library(dplyr)
+  
+  ########read outcome data 
+  csv <- read.csv("outcome-of-care-measures.csv", stringsAsFactors = FALSE, 
+                  header = TRUE, na.strings = "Not Available") ##reading the data 
+  table <- data.frame(csv [, 2], #"hospital"
+                      csv [, 7], #"state"
+                      csv [, 11]) #"heart attack"
+  colnames(table) <- c("hospital", "state", "heart attack") ##selecting columns 
+  table_1 <- table[!(table$`heart attack` == "Not Available"), ] ##changing Not Avilables to NA 
+  table_2 <- na.omit(table_1) ##removing all the NAs 
+  
+  ########check the state and outcome are valid 
+  if(!outcome %in% "heart attack"){
+    stop('invalid outcome')
+  }
+  
+  ########Return hospital name in that state with the given rank 
+  
+  #create a list of states to iterate over. 
+  state_list = unique(table_2$state)
+  state_list = as.list(state_list) #we created a list of 54 
+          #print(state_list)
+  
+  #re-order the list via priority of state -> heart attack -> hospital 
+  table_3 <- table_2[order(table_2$state, table_2$`heart attack`, table_2$hospital), ]
+  
+  #n is a large list with 54 elements, each element is a data frame with nrows and 3 columns 
+  new_list <- split(table_3, table_3$state) 
+  
+  #create an empty vector
+  o <- vector()
+  
+  #use for loop to iterate over each state, split the table by state, 
+  #and extract the rank 1 hospital (i.e., row 1)
+  #for (i in state_list) {print(i)} iterates over the 50 state names 
+  for (i in 1:length(state_list)) {
+    #print(i)
+    n <- as.data.frame(new_list[i])
+    m <- n[1, ]
+    o[i] <- m
+  }
+  #turn these two lists into table columns 
+  hospital <- data.frame(Reduce(rbind, o))
+  #print(hospital)
+  state <- data.frame(Reduce(rbind, state_list))
+  #print(state)
+  #coerce the column 
+  final <- cbind(hospital, state)
+  colnames(final) <- c("hospital", "state")
+  print(final)
+}
